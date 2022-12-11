a954865961a112f4bebf2b1989812359747e7721
Cresencio <cresenciocantu@gmail.com>
2022-12-10 19:27:21 -0600

    Update extension detauls

---
+++ scripts/contextMenuServiceWorker.js
@@ -0,0 +1,106 @@
+// Function to get + decode API key
+const getKey = () => {
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.get(['openai-key'], (result) => {
+        if (result['openai-key']) {
+          const decodedKey = atob(result['openai-key']);
+          resolve(decodedKey);
+        }
+      });
+    });
+  };
+
+  const sendMessage = (content) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const activeTab = tabs[0].id;
+  
+      chrome.tabs.sendMessage(
+        activeTab,
+        { message: 'inject', content },
+        (response) => {
+          if (response.status === 'failed') {
+            console.log('injection failed.');
+          }
+        }
+      );
+    });
+  };
+
+const generate = async (prompt) => {
+  // Get your API key from storage
+  const key = await getKey();
+  const url = 'https://api.openai.com/v1/completions';
+	
+  // Call completions endpoint
+  const completionResponse = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${key}`,
+    },
+    body: JSON.stringify({
+      model: 'text-davinci-003',
+      prompt: prompt,
+      max_tokens: 1250,
+      temperature: 0.7,
+    }),
+  });
+	
+  // Select the top choice and send back
+  const completion = await completionResponse.json();
+  return completion.choices.pop();
+}
+
+    const generateCompletionAction = async (info) => {
+
+      // Send mesage with generating text (this will be like a loading indicator)
+    sendMessage('generating...');
+
+        try {
+          const { selectionText } = info;
+          const basePromptPrefix = `
+      Craft a 90-second elevator pitch for a startup idea. The pitch should describe the problem, solution, how it works, and the added benefit or value.
+      
+      Pitch:
+      `;
+      
+          const baseCompletion = await generate(
+            `${basePromptPrefix}${selectionText}`
+          );
+      
+          // I build Prompt #2.
+        const secondPrompt = 
+        `
+        Take the startup idea and base pitch below and rewrite it with a more conversational tone. The pitch should be persuasive and appeal to emotion.
+
+        Startup idea: ${selectionText}
+
+        Base pitch : ${baseCompletion.text}
+
+        Rewritten pitch:
+        `
+      
+          // Call your second prompt
+          const secondPromptCompletion = await generate(secondPrompt);
+
+            // Send the output when we're all done
+      sendMessage(secondPromptCompletion.text);
+    } catch (error) {
+      console.log(error);
+  
+      // Add this here as well to see if we run into any errors!
+      sendMessage(error.toString());
+    }
+  };
+
+// Add this in scripts/contextMenuServiceWorker.js
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: 'context-run',
+      title: 'Generate pitch',
+      contexts: ['selection'],
+    });
+  });
+  
+  // Add listener
+  chrome.contextMenus.onClicked.addListener(generateCompletionAction);
